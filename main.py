d5cea0393dc130502161ef72f4204656421f09f6
madegwa-o <madegwaoscar317@gmail.com>
2025-11-04 11:13:32 +0300

    this is the initial project setup

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
-from api import authtoken
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    """Call the /get_token route and return the token."""
-    token_response =  authtoken.get_token()
-    return {"message": "Daraja Access Token", "data": token_response}
+    return {"message": "Daraja Access Token", "data": "Hello world"}
 
 
 
-@app.get("/get_token")
-def get_access_token():
-    """Fetch OAuth access token from Safaricom Daraja API."""
-    token_response = authtoken.get_token()
-    return {"message": "Daraja Access Token", "data": token_response}
