b2cad404eade376ac3ffc0582a2ae910716c8973
madegwa-o <madegwaoscar317@gmail.com>
2025-11-04 09:57:13 +0300

    fully consumed auth api

---
+++ api/authtoken.py
@@ -0,0 +1,33 @@
+import base64
+import json
+from wsgiref import headers
+
+import requests
+
+AUTH_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+CONSUMER_KEY = "reJjt6xnyvZdACLrlVvtI8GjCuUcIT5zWyq2xm5GzU4Np1nb"
+CONSUMER_SECRET = "AvH0jMC0pLNnCekwUCfvG0ltXJ1kyZ1GzEA724H0KY9wnWKPU6QqC6h250FDxn5r"
+
+def get_auth_token():
+    try:
+
+        credentials = f'{CONSUMER_KEY}:{CONSUMER_SECRET}'
+        encoded_credentials = base64.b64encode(credentials.encode()).decode()
+
+        headers = {
+            "Authorization": f"Basic {encoded_credentials}"
+        }
+
+        response = requests.get(AUTH_URL, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()
+
+        return {
+            "error code": response.status_code,
+            "error message": response.text,
+        }
+
+    except Exception as e:
+        return json.dumps({"error": str(e)})
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from api import authtoken
 app = FastAPI()
 
 @app.get("/")
@@ -7,4 +7,10 @@ def root():
     return {"message": "Daraja Access Token", "data": "Hello world"}
 
 
+
+
+@app.get('/get_token')
+def get_token():
+    token_res = authtoken.get_auth_token()
+    return {"message": "Daraja Access Token", "data": token_res}
 
